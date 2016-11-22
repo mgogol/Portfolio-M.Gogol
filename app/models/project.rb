f97fcf6803f82cb3f421dde53306e8bd9b3e3682
mgogol <m.gogol.dev@gmail.com>
2016-11-22 22:03:12 +0200

    Added styles to portfolio element page.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ActiveRecord::Base
-  has_attached_file :image, styles: { medium: "200x200>", thumb: "200x200>" }
+  has_attached_file :image, styles: { medium: "800x600>", thumb: "250x250>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
