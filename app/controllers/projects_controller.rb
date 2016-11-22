09d23a6cac7a556cffeb3b0231da836bac5b7a20
mgogol <m.gogol.dev@gmail.com>
2016-11-22 13:12:44 +0200

    Added Project CRUD

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,6 @@
 class ProjectsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
@@ -9,14 +11,37 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new project_params
     if @project.save
-      redirect_to @project, notice: "Hey, Mykola! Your project is successfully saved."
+      redirect_to @project, notice: 'Hey, Mykola! Your project is successfully saved.'
     else
-      render 'new', notice: "Oh, no! I was unable to save your project."
+      render 'new', notice: 'Oh, no! I was unable to save your project.'
     end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update project_params
+      redirect_to @project, notice: 'Your project successfully updated!'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to project_path
   end
 
   private
   def project_params
     params.require(:project).permit(:title, :description)
+  end
+
+  def find_post
+    @project = Project.find(params[:id])
   end
 end
