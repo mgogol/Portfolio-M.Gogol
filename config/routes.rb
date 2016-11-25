78c2d2e398c0ec47fcd5f511583b65ad4d5611fb
mgogol <m.gogol.dev@gmail.com>
2016-11-25 14:08:47 +0200

    Fixed fails routes to contact#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :projects
+  match '/contacts',     to: 'contacts#new',             via: 'get'
   resources :contacts, only: [:new, :create]
   get 'welcome/index'
   root 'welcome#index'
