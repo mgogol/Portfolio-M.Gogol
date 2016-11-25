95770dd287c62aae683ecf4b53463d0f6239dabf
mgogol <m.gogol.dev@gmail.com>
2016-11-25 14:21:54 +0200

    Fixed Contact Form routes bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :projects
-  match '/contacts',     to: 'contacts#new',             via: 'get'
   resources :contacts, only: [:new, :create]
   get 'welcome/index'
   root 'welcome#index'
