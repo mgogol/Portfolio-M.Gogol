fa71e75abfda3e8ce7d839be125e5c143ed97735
mgogol <m.gogol.dev@gmail.com>
2016-11-26 12:00:09 +0200

    Added styles to sign_in form.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :contacts, only: [:new, :create]
   get 'welcome/index'
   root 'welcome#index'
+  # get 'sign_in', to: 'devise/sessions#new'
+
+  get '*path' => redirect('/')
 end
