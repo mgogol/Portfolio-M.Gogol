7114ef5a53ee8b87c0393f0b9fa56f168db6951e
mgogol <m.gogol.dev@gmail.com>
2016-11-27 15:46:17 +0200

    Aded media queries for responsive web design.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  has_attached_file :image, styles: { medium: "100%x700", thumb: "260x260#" }
+  has_attached_file :image, styles: { medium: "100%x700", thumb: "390x390#" }
                     # convert_options: { thumb: "-gravity north -thumbnail 260x260^ -extent 260x260" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
