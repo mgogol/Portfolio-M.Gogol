7e3fed6cf65f353e2d5613d7b0c8b05225cf9d91
mgogol <m.gogol.dev@gmail.com>
2016-12-03 12:32:16 +0200

    Added Amazon S3 for Paperclip image uploading to Heroku

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "100%x700", thumb: "" },
                     convert_options: { thumb: "-gravity north -thumbnail 320x320^ -extent 320x320" }
-                    # convert_options: { thumb: "-gravity north -thumbnail 260x260^ -extent 260x260" }
+                    # path: "/:class/:attachment/:id_partition/:style/:filename"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   extend FriendlyId
