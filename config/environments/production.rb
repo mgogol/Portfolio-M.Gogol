f7dfa98d2996be5bbfe46a5c6d3fe538d6fdd41e
mgogol <m.gogol.dev@gmail.com>
2016-12-03 11:58:10 +0200

    Added Amazon S3 for Paperclip image uploading to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,5 +42,13 @@ Rails.application.configure do
       :authentication => :plain,
       :enable_starttls_auto => true
   }
-
+  
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV['AWS_BUCKET'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
