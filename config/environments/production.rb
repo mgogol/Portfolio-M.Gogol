93ad4c176ce7212e7abae761eff3093ce5eca87f
mgogol <m.gogol.dev@gmail.com>
2016-12-11 11:16:55 +0200

    Minifying CSS to Google insights rules.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,6 +33,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://radiant-earth-85759.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
+  config.assets.css_compressor = :sass
+
   ActionMailer::Base.smtp_settings = {
       :user_name => ENV['SENDGRID_USERNAME'],
       :password => ENV['SENDGRID_PASSWORD'],
